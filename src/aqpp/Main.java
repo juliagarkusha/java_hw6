708a90021f59969c6a192db3d0903018d0e8ba71
julia <julija@flashphoner.com>
2024-08-15 14:37:43 +0300

    first commit

---
+++ src/aqpp/Main.java
@@ -0,0 +1,22 @@
+package aqpp;
+
+public class Main {
+    static int count;
+    static int sum;
+    static int[] arr;
+
+    public static void main(String[] args) {
+        arr = new int[]{1, 2, 3, 4, 5, 6};
+        sum = 0;
+        count = 0;
+
+        for (int arrItem: arr) {
+            sum += arrItem;
+            count++;
+
+            System.out.println(count + ") Num is " + arrItem + ", sum is " + sum);
+        }
+
+        System.out.println("Sum of numbers is " + sum);
+    }
+}
